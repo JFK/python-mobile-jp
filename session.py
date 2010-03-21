9e1d956064bd9d71bd8ce0a948732fbd8a3369c7
fumikazu.kiyota <fumikazu.kiyota@7eb932cc-2ec3-11df-8474-0f70896a4c6b>
2010-03-21 01:43:24 +0000

    fix lifetime

--- session.py
+++ session.py
@@ -25,23 +25,26 @@ from pymongo import Connection, ASCENDING
 class start:
 
     def __init__(self, **kwargs):
-        self.authdb = 'sessiondb.transaction' if 'sessiondb' not in kwargs else kwargs['sessiondb']
-        self.keyname = '_tk' if 'keyname' not in kwargs else kwargs['keyname']
-        self.keylength = 128 if 'keylength' not in kwargs else kwargs['keylength']
-        self.lifetime = 31 * 86400 if 'lifetime' not in kwargs else kwargs['lifetime']
-        self.session = None
+        self._authdb = 'sessiondb.transaction' if 'sessiondb' not in kwargs else kwargs['sessiondb']
+        self._keyname = '_tk' if 'keyname' not in kwargs else kwargs['keyname']
+        self._keylength = 128 if 'keylength' not in kwargs else int(kwargs['keylength'])
+        self._lifetime = 31 * 86400 if 'lifetime' not in kwargs else int(kwargs['lifetime'])
+        self._session = None
 
         con = Connection()
-        self._con = eval('con.' + self.authdb)
+        self._con = eval('con.' + self._authdb)
         self._gc()
 
     def _token(self):
         letters=string.ascii_letters+string.digits
-        rnd = ''.join([random.choice(letters) for _ in range(self.keylength)])
+        rnd = ''.join([random.choice(letters) for _ in range(self._keylength)])
         return base64.b64encode(''.join([str(int(time.time())),rnd]))
 
     def _gc(self):
-        self._con.remove({'time':{'$gt':time.time() - self.lifetime}})
+        self._con.remove({'time':{'$gt':int(time.time()) - self._lifetime}})
+
+    def keyname(self):
+        return self._keyname
 
     def remove(self):
         self._con.remove({'key':self.key()})
@@ -56,9 +59,9 @@ class start:
     def lock(self, key):
         token = self._token()
         self._con.remove({'key':key})
-        self._con.insert({'key':key,'token':token,'time':str(int(time.time()))})
+        self._con.insert({'key':key,'token':token,'data':None,'time':str(int(time.time()))})
         self._con.create_index([("key", ASCENDING)])
-        self.session = "|".join([base64.b64encode(key),token])
+        self._session = "|".join([base64.b64encode(key),token])
 
     def open(self, value, **kwargs):
         if not value: return None
@@ -66,30 +69,32 @@ class start:
         if len(parts) != 2: return None
         key = base64.b64decode(parts[0])
         result = self._con.find_one({"key":key})
+        print  result['time']
+        print  int(time.time()) - self._lifetime
         if result and 'token' in result and result['token'] == parts[1] \
-             and result['time'] >= time.time() - self.lifetime:
+             and int(result['time']) > int(time.time()) - self._lifetime:
             if 'renew' in  kwargs and kwargs['renew']:
                 token = self._token()
                 self._con.update({"key":key},
                     {'$set': {'token':token,'time':str(int(time.time()))}})
-                self.session = "|".join([parts[0], token])
+                self._session = "|".join([parts[0], token])
             else:
                 self._con.update({"key":key},
                     {'$set': {'time':str(int(time.time()))}})
-                self.session = "|".join([parts[0], parts[1]])
+                self._session = "|".join([parts[0], parts[1]])
             return True
         else:
             self._con.remove({'key':key})
             return False
 
     def key(self):
-        if not self.session: return None
-        parts = self.session.split("|")
+        if not self._session: return None
+        parts = self._session.split("|")
         if len(parts) != 2: return None
         return base64.b64decode(parts[0])
 
     def urlencode(self):
-        return urllib.urlencode({self.keyname:self.session})
+        return urllib.urlencode({self._keyname:self._session})
 
     def form_html(self):
-        return '<input type="hidden" name="' + self.keyname + '" value="' + self.session + '"/>'
+        return '<input type="hidden" name="' + self._keyname + '" value="' + self._session + '"/>'
